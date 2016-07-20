d302447e328f4017f7b3bd1342a5bf9d717aa39e
Piotr Szymański <niedakh@gmail.com>
2016-07-20 14:05:36 +0200

    Batch processing (#1)
    Merging batch processing support

--- docker/assets/run.sh
+++ docker/assets/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mono /obeliks/PosTaggerTag.exe -lem:/obeliks/models/LemmatizerModel.bin  -o -t /data.txt /obeliks/models/TaggerMar2013.bin /data.xml &> /dev/null
+mkdir -p /xml
+mkdir -p /out/xml
+# ls /input/
+# ls -R /out/
+mono /obeliks/PosTaggerTag.exe  -t -o -lem:/obeliks/models/LemmatizerModel.bin "/input/*.txt" /obeliks/models/TaggerMar2013.bin /xml/ &> /output
 
-cat /data.xml
-
-cp /data.xml /out/
+cat /output
+mv /output /out/
+mv /xml/* /out/xml
